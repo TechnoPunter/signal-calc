b54925480579776ec1b889c59d1056ae903892ea
mahipratishthan <mahi@pratishthanventures.com>
2023-11-02 22:21:36 +0530

    added models and sample endpoints

---
+++ models.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+
+# features
+# 1. It's just plain python
+# 2. Built in async
+# 3. Built in data validation using pydantic
+# 4. Typed python
+# 5. Errors in json
+# 6. Authentication built in
+# 7. Swagger and ReDoc built-in
+
+class Todo(BaseModel):
+    title: str
+    description: str
+
+
+class SignalReq(BaseModel):
+    ltp: str
+    scrip: str
+
+
+class SignalResp(BaseModel):
+    signal: str
+
+
+class Signal(BaseModel):
+    scrip: str
+    low: float
+    high: float
